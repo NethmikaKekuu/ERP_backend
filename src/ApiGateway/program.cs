3ecfb1b75d38c299332c76a337a99553a3861a21
san-rat <ratnayake.sanuk@gmail.com>
2026-03-02 22:44:23 +0530

    Feat: add ocelot.Production.json with Azure internal hostnames.

--- src/ApiGateway/program.cs
+++ src/ApiGateway/program.cs
@@ -32,6 +32,7 @@ try
         .AddJsonFile("appsettings.json",                                          optional: false, reloadOnChange: true)
         .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json",   optional: true)
         .AddJsonFile("ocelot.json",                                               optional: false, reloadOnChange: true)
+        .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json",        optional: true,  reloadOnChange: true)
         .AddEnvironmentVariables();
 
     // ── JWT settings ──────────────────────────────────────────────────────────
